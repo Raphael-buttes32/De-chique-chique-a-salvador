7b1642a9017705ee5573214d13e20c65ea150f97
Raphael-buttes32 <raphael.buttes@escola.pr.gov.br>
2025-10-30 09:25:06 -0300

    ssasds

--- main.js
+++ main.js
@@ -5,11 +5,13 @@ avanca.forEach(button => {
       button.addEvenveListeener('click', function () {
             const atual = document.querySelector('.a')
             const proximoPasso = "passo-" + this.gatAttribute('data-proximo')
+         atual.classList.remove('ativo')  
+         document.getElementById(proximoPasso).classList.add('ativo')
       })
 }
 
 )
 
-
+d
 
 
